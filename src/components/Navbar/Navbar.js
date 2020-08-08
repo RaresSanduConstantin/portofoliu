28ec5014476077980d1d4f2ce28693a4fcad8e45
RaresSanduConstantin <59281487+RaresSanduConstantin@users.noreply.github.com>
2020-08-08 21:28:12 +0300

    start

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+export class Navbar extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <h1>NavBar</h1>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
---
+++ src/firebase/firebase.js
@@ -0,0 +1,14 @@
+import firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+
+const config = {
+  apiKey: "AIzaSyCYL0va33N4EmJSLGr0Oy-j6c2zjcTGn3Y",
+  authDomain: "portofoliu-53fba.firebaseapp.com",
+  databaseURL: "https://portofoliu-53fba.firebaseio.com",
+  projectId: "portofoliu-53fba",
+  storageBucket: "portofoliu-53fba.appspot.com",
+  messagingSenderId: "152052172457",
+  appId: "1:152052172457:web:907d4e0dd0fcb987244bb8",
+  measurementId: "G-9MS9FWH6YR",
+};
