7045f0d508c6fea55926af67a109225429f44af3
RaresSanduConstantin <59281487+RaresSanduConstantin@users.noreply.github.com>
2020-08-28 20:28:31 +0300

    ceva pe work

--- src/pages/WorkPage/Work.js
+++ src/pages/WorkPage/Work.js
@@ -9,16 +9,18 @@ const Work = () => {
       {INITIAL.map(({ id, ...otherProps }) => {
         return (
           <div key={id} className="card-component">
-            <div className="imagini-proiect">
+            <div className="imagini-card">
               <img
                 src={otherProps.imageUrl}
                 alt={otherProps.title}
                 style={{ width: "300px" }}
               />
             </div>
-            <p>{otherProps.title}</p>
-            <a href={otherProps.githubLink}>View CodeBase</a>
-            <a href={otherProps.liveAppLink}>View LiveApp!</a>
+            <div className="text-card">
+              <p>{otherProps.title}</p>
+              <a href={otherProps.githubLink}>View CodeBase</a>
+              <a href={otherProps.liveAppLink}>View LiveApp!</a>
+            </div>
           </div>
         );
       })}
