d6392f53b14c459fa83461adf272b1b5b9754474
RaresSanduConstantin <59281487+RaresSanduConstantin@users.noreply.github.com>
2020-09-06 14:13:55 +0300

    plm

--- src/pages/Admin/Admin.js
+++ src/pages/Admin/Admin.js
@@ -5,16 +5,25 @@ import "./Admin.css";
 class Admin extends Component {
   constructor(props) {
     super(props);
-    this.state = { headerTyping: "sssss", descriere: "" };
+    this.state = { typingDesc: "", description: "" };
+
     this.returnData();
+
+    this.adminMesaj = React.createRef();
   }
 
-  handleChange(event) {
+  handleTypingChange = (event) => {
     //handles change in the input's value
-    this.setState({ value: event.target.value });
-  }
+    this.setState({ typingDesc: event.target.value });
+  };
 
-  handleSubmit(e) {
+  handleDescriptionChange = (event) => {
+    //handles change in the input's value
+    this.setState({ description: event.target.value });
+  };
+
+  handleSubmitAdmin(e) {
+    console.log(this.state.typingDesc, this.state.description);
     e.preventDefualt();
   }
 
@@ -32,22 +41,70 @@ class Admin extends Component {
       <div className="containerForm">
         <form>
           <div className="desc">
-            <p>
+            <h1 className="h1">Header Page</h1>
+            <p className="p">
               <label>Typing description:</label>
             </p>
             <input
               id="typintDesc"
               type="text"
               placeholder="typind description"
+              value={this.typingDesc}
+              onChange={this.handleTypingChange}
             />
-          </div>
-          <div className="desc">
-            <p>
+
+            <p className="p">
               <label>Description:</label>
             </p>
-            <input id="description" type="text" placeholder="Description" />
+            <input
+              id="description"
+              type="text"
+              placeholder="Description"
+              value={this.description}
+              onChange={this.handleDescriptionChange}
+            />
+            <h1 className="h1">Work Page</h1>
+            <p className="p">
+              <label>Update Work:</label>
+            </p>
+            <input
+              className="inputWork"
+              id="imgUrl"
+              type="text"
+              placeholder="imgUrl"
+            />
+            <input
+              className="inputWork"
+              id="titleUrl"
+              type="text"
+              placeholder="titleUrl"
+            />
+            <input
+              className="inputWork"
+              id="descriptionUrl"
+              type="text"
+              placeholder="descriptionUrl"
+            />
+            <input
+              className="inputWork"
+              id="viewCodeBase"
+              type="text"
+              placeholder="viewCodeBase"
+            />
+            <input
+              className="inputWork"
+              id="viewAppLive"
+              type="text"
+              placeholder="viewAppLive"
+            />
+            <h1 className="h1">Contact Mesaj</h1>
+            <div className="contactMesaj">
+              <div></div>
+            </div>
+            <button onClick={this.handleSubmitAdmin} className="buttonSubmit">
+              Submit
+            </button>
           </div>
-          <button className="example_d">Submit</button>
         </form>
       </div>
     );
